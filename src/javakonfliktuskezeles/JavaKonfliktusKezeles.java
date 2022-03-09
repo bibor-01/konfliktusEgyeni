05e90faecfb01c737b44c8a4ed0634e20d52c997
bibor-01 <balogh01biborka@gmail.com>
2022-03-09 11:48:25 +0100

    1. név inicializálása

--- src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -4,7 +4,7 @@ package javakonfliktuskezeles;
 public class JavaKonfliktusKezeles {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1="bíbor";
         String nev2;
     }
     
